077db15b8a85befd98d48293adf4c44b91efff57
Tikin686 <sergey.tikin94@gmail.com>
2024-11-09 21:30:01 +0300

    init

---
+++ blog/views.py
@@ -0,0 +1,53 @@
+from blog.models import Blog
+from django.views.generic import ListView, DeleteView, UpdateView, CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from blog.forms import BlogForm
+from pytils.translit import slugify
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = "blog/blog_list.html"
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    form_class = BlogForm
+    success_url = reverse_lazy("blog:blog_list")
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("blog:blog_list")
